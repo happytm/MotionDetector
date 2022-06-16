81b620fcaa2cb67991c2ad4e8f8ac390bbb6a004
happytm <36576353+happytm@users.noreply.github.com>
2022-06-16 00:26:53 -0400

    Update motionDetector_simple.ino

--- motionDetector_simple.ino
+++ motionDetector_simple.ino
@@ -15,26 +15,22 @@ void setup(){
   motionDetector_config(64, 16, 3, 3, false); 
   Serial.setTimeout(1000);
 
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Wifi connection failed");
-    WiFi.disconnect(false);
-    delay(1000);
-    WiFi.begin(ssid, password);
-   }
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {Serial.println("Wifi connection failed");WiFi.disconnect(false);delay(1000);WiFi.begin(ssid, password);}
+
 }  // End of setup.
 
 void loop()
 { 
  motionDetector_set_minimum_RSSI(-80);     // Minimum RSSI value to be considered reliable. Default value is 80 * -1 = -80.
  motionLevel = 0;                          // Reset motionLevel to 0 to resume motion tracking.
- motionLevel = motionDetector_esp();       // if the connection fails, the radar will automatically try to switch to different operating modes by using ESP32 specific calls. 
+ motionLevel = motionDetector_esp();       // if the connection fails, the motion detector will automatically try to switch to different operating modes by using ESP32 specific calls. 
  //Serial.print("Motion detected & motion level is: ");Serial.println(motionLevel);
 
  if (motionLevel > motionThreshold)        // Adjust the sensitivity of motion sensor.Higher the number means less sensetive motion sensor is.
  {Serial.print("Motion detected & motion level is: ");Serial.println(motionLevel);}
   
- if (enableCSVgraphOutput > 0) {           // Use Tools->Serial Plotter to graph the live data!
-      motionDetector_enable_serial_CSV_graph_data(enableCSVgraphOutput); // output CSV data only
-      }
+ if (enableCSVgraphOutput > 0)             // Use Tools->Serial Plotter to graph the live data!
+ {motionDetector_enable_serial_CSV_graph_data(enableCSVgraphOutput);}   // output CSV data only
+      
   delay(500);
 } // End of loop.
