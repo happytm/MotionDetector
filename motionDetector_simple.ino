5f3736a73cf8aa476754cd6a1198a52801e70af2
happytm <36576353+happytm@users.noreply.github.com>
2022-06-16 00:05:26 -0400

    Update motionDetector_simple.ino

--- motionDetector_simple.ino
+++ motionDetector_simple.ino
@@ -1,12 +1,11 @@
 #include <WiFi.h>
 #include "motionDetector.h"    // Thanks to https://github.com/paoloinverse/motionDetector_esp
 
-const char* ssid = "ESP";      // Required for OTA update.
-const char* password = "";     // Required for OTA update.
-
-int enableCSVgraphOutput = 1;  // 0 disable, 1 enable // if enabled, you may use Tools-> Serial Plotter to plot the variance output for each transmitter. 
+const char* ssid = "ESP";      
+const char* password = "";     
+int enableCSVgraphOutput = 1;  // 0 disable, 1 enable.If enabled, you may use Tools-> Serial Plotter to plot the variance output for each transmitter. 
 int motionLevel = -1;
-//==================User configuration generally not required below this line ============================
+int motionThreshold = 15;      // Threshhold above which motion is considered detected.
 
 void setup(){
   Serial.begin(115200);
@@ -22,18 +21,16 @@ void setup(){
     delay(1000);
     WiFi.begin(ssid, password);
    }
-}
-
-//===================== End of Setup ====================================================
+}  // End of setup.
 
 void loop(){ 
   
  motionDetector_set_minimum_RSSI(-80);     // Minimum RSSI value to be considered reliable. Default value is 80 * -1 = -80.
- motionLevel = 0;  // Reset motionLevel to 0 to resume motion tracking.
- motionLevel = motionDetector_esp();  // if the connection fails, the radar will automatically try to switch to different operating modes by using ESP32 specific calls. 
+ motionLevel = 0;                          // Reset motionLevel to 0 to resume motion tracking.
+ motionLevel = motionDetector_esp();       // if the connection fails, the radar will automatically try to switch to different operating modes by using ESP32 specific calls. 
  //Serial.print("Motion detected & motion level is: ");Serial.println(motionLevel);
 
- if (motionLevel > 15)  // Adjust the sensitivity of motion sensor.Higher the number means less sensetive motion sensor is.
+ if (motionLevel > motionThreshold)        // Adjust the sensitivity of motion sensor.Higher the number means less sensetive motion sensor is.
  {     
   Serial.print("Motion detected & motion level is: ");Serial.println(motionLevel);
   delay(500);
